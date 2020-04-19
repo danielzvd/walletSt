ed72c6f438b0a895b113dbd24cccdeafd98ed645
danielzvd <danieldeazevedosilva@gmail.com>
2020-04-19 00:44:19 -0300

    configurando statusbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import StackNavigator from '!/navigators/StackNavigator';
 
 const App = () => {
+  useEffect(() => {
+    StatusBar.setBackgroundColor('#5372ff', true);
+    StatusBar.setBarStyle('light-content', true);
+  }, []);
   return (
     <NavigationContainer>
       <StackNavigator />
