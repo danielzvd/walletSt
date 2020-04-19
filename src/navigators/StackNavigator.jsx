b709d742f9882a5f679cc5103ecf2b8faed69a48
danielzvd <danieldeazevedosilva@gmail.com>
2020-04-19 00:09:37 -0300

    adicionando materialDesingIcons e header

--- src/navigators/StackNavigator.jsx
+++ src/navigators/StackNavigator.jsx
@@ -1,15 +1,62 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import Welcome from '!/screens/Welcome/Welcome';
 import Login from '!/screens/Login/Login';
-
+import logoHorizontal from '!/assets/images/logo-horizontal.png';
 const Stack = createStackNavigator();
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const StackNavigator = () => {
+const StackNavigator = ({ navigation }) => {
+  const screenOptions = ({ navigation }) => ({
+    headerStyle: {
+      backgroundColor: '#5372ff',
+    },
+    headerRightContainerStyle: {
+      zIndex: 99,
+    },
+    headerLeftContainerStyle: {
+      zIndex: 99,
+    },
+    headerTitle: () => (
+      <Image
+        source={logoHorizontal}
+        style={{
+          width: 270,
+          height: 40,
+          position: 'absolute',
+          left: -30,
+          top: -20,
+        }}
+      />
+    ),
+    headerLeft: () => (
+      <TouchableOpacity onPress={() => false}>
+        <Icon name="menu" size={32} color="#FFFFFF" style={{ padding: 15 }} />
+      </TouchableOpacity>
+    ),
+    headerRight: () => (
+      <TouchableOpacity onPress={() => false}>
+        <Icon name="bell" size={28} color="#FFFFFF" style={{ padding: 15 }} />
+      </TouchableOpacity>
+    ),
+  });
+
   return (
-    <Stack.Navigator initialRouteName="Welcome">
-      <Stack.Screen name="Welcome" component={Welcome} />
+    <Stack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#5372ff',
+        },
+        headerTintColor: '#fff',
+      }}>
+      <Stack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={screenOptions}
+      />
       <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
